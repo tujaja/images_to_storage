fc20e3456f4512bd52294d96d367ec4234d1353a
tujaja <tujaja2@gmail.com>
2010-09-26 00:12:21 +0900

    ...

--- install.rb
+++ install.rb
@@ -1,6 +1,2 @@
 # Install hook code here
-require File.expand_path(
-  File.join(File.dirname(__FILE__), '../../../config/environment')
-)
-
-FileUtils.cp  "./images_storage.yml", "#{RAILS_ROOT}/config"
+FileUtils.cp  "./images_storage.yml", "../../../config/"
